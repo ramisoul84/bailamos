05a8dd6996dba9a18c127037dc5869fefed7f5bd
ramis <111607770+ramisoul84@users.noreply.github.com>
2024-07-13 13:46:35 +0300

    first commitinitial

---
+++ src/app/components/account/account.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+  declarations: [LoginComponent, RegisterComponent],
+})
+export class AccountModule {}
